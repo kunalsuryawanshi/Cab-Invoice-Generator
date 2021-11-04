27d878048259e057eae453bea34fbe6313e035d3
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-11-04 22:43:02 +0530

    Added: Test Cases and Code for Given Distance and Time Return Total Fare

---
+++ src/test/java/com/invoiceservice/InvoiceServiceTest.java
@@ -0,0 +1,20 @@
+package com.invoiceservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fare = invoiceGenerator.calculateFare(2.0, 5);
+        Assertions.assertEquals(25.0, fare);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double fare = invoiceGenerator.calculateFare(0.1, 1);
+        Assertions.assertEquals(5, fare);
+    }
+}
