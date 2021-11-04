8007665db05e4333b6186f5e0c5303fc0dac4dcd
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-11-04 23:45:37 +0530

    Refactored: Test Cases For Multiple Rides

--- src/test/java/com/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/invoiceservice/InvoiceServiceTest.java
@@ -1,26 +1,30 @@
 package com.invoiceservice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+    @BeforeEach
+    void setUp() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double fare = invoiceGenerator.calculateFare(2.0, 5);
         Assertions.assertEquals(25.0, fare);
     }
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double fare = invoiceGenerator.calculateFare(0.1, 1);
         Assertions.assertEquals(5, fare);
     }
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
