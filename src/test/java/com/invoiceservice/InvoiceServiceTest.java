cd38b62b0efea9fe12e471cc28a277d202197ecd
kunalsuryawanshi <kunalsuryawanshi25@gmail.com>
2021-11-05 23:01:43 +0530

    Added: Test Case and Code For UserID and Rides from RideRepository, Return Invoice

--- src/test/java/com/invoiceservice/InvoiceServiceTest.java
+++ src/test/java/com/invoiceservice/InvoiceServiceTest.java
@@ -35,11 +35,20 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummary() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
         InvoiceSummary invoiceSummary = invoiceGenerator.calculateFareSummary(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+
+    @Test
+    public void givenUserIdAndRide_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        invoiceGenerator.addRides("abc101", rides);
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary("abc101");
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assertions.assertEquals(expectedInvoiceSummary, invoiceSummary);
     }
